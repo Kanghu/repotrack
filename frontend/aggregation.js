29de7403846279b73f3d84b9b463e890acca8766
Kanghu <kanghu_628@yahoo.com>
2021-07-28 11:02:30 +0300

    Aggregation => Incorporate time-mode & some additional sanity checks

--- frontend/aggregation.js
+++ frontend/aggregation.js
@@ -3,14 +3,17 @@ Configs = [".xml", ".yaml", ".md", ".gitignore", ".ini", ".cfg", ".json"]
 // Compute aggregated metrics for a certain contributor
 function computeAggregatedMetrics(contribution_map_self, contribution_map_all) {
 	metrics = {}
+
 	// Aggregation methods
 	const fwd = function(loc, func) { return loc+func }
 	const re = function(func_1, func_2) { return func_1 + func_2 }
 	const man = function(com, loc) { return com+loc }
 
+	//
 	cfg_sum_self = Object.keys(contribution_map_self).filter(k => Configs.includes(k)).map(k => contribution_map_self[k]).reduce((a, b) => a + b, 0)
 	cfg_sum_all = Object.keys(contribution_map_all).filter(k => Configs.includes(k)).map(k => contribution_map_all[k]).reduce((a, b) => a + b, 0)
 
+	// Call the methods defined above on given parameters
 	metrics['Engineering'] = fwd(contribution_map_self['LOC+'], contribution_map_self['FUNC+']) / fwd(contribution_map_all['LOC+'], contribution_map_all['FUNC+'])
 	metrics['Re-engineering'] = re(contribution_map_self['FUNC+'], contribution_map_self['FUNC*']) / re(contribution_map_all['FUNC+'], contribution_map_all['FUNC*'])
 	metrics['Management'] =
@@ -24,17 +27,34 @@ function computePackageStats(node) {
 	// Variables for counting children & computing the average
 	avg = {}
 	children = node.children ? node.children : node._children
+	len_childs = children.length
 
+	// Parse all childs objects of this node
 	for(i=0; i<children.length; i++) {
 			contrib = {}
 			top10 = node.contributors.length / 10
-			all_contribution_map = children[i].contributors.filter(c => c.name == 'All').map(c => c.contrib)[0]
 
+			// Collect the summed contribution map for all contributors according to selected time mode
+			all_contribution_map = null;
+			if(timeMode == 'recent') {
+				all_contribution_map = children[i].contributors.filter(c => c.name == 'All').map(c => c.contrib_recent)[0]
+				children[i].contributors.sort((a, b) => (a.contrib_recent['LOC+'] > b.contrib_recent['LOC+']) ? -1 : 1)
+			} else {
+				all_contribution_map = children[i].contributors.filter(c => c.name == 'All').map(c => c.contrib)[0]
+				children[i].contributors.sort((a, b) => (a.contrib['LOC+'] > b.contrib['LOC+']) ? -1 : 1)
+			}
+
+			// Parse the top-10% contributors of this node
 			for(j=1; j<1+Math.min(node.contributors.length-1, Math.ceil(top10)); j++) {
 					if(j >= children[i].contributors.length) break;
 					else if(children[i].contributors[j] == undefined) continue;
 
-					contribution_map = children[i].contributors[j].contrib
+					contribution_map = null
+					if(timeMode == 'recent') {
+							contribution_map = children[i].contributors[j].contrib_recent
+					} else {
+							contribution_map = children[i].contributors[j].contrib
+					}
 
 					for(const stat in contribution_map) {
 							if(!(stat in contrib)) {
@@ -44,8 +64,12 @@ function computePackageStats(node) {
 							// We check for NaN values beforehand
 							self_term = contribution_map[stat] != NaN ? contribution_map[stat] : 0;
 							all_term = all_contribution_map[stat] != NaN ? all_contribution_map[stat] : 0;
-							contrib[stat] += 1.0 * (self_term) / (all_term + 1)
+							contrib[stat] += 1.0 * (self_term) / (all_term > 0 ? all_term : 1)
 					}
+			}
+
+			if(contrib['LOC+'] == 0) {
+				len_childs -= 1
 			}
 
 			// Store the running average for each children
@@ -56,14 +80,16 @@ function computePackageStats(node) {
 
 					avg[stat] += contrib[stat]
 			}
+
+			console.log(contrib['LOC+'])
 	}
 
 	console.log(avg)
 	// Divide back to obtain average
 	for(const stat in avg) {
-			avg[stat] = avg[stat] / children.length
+			avg[stat] = avg[stat] / len_childs
 			avg[stat] = avg[stat] * 100.0
-			
+
 			if(avg[stat] > 100.0) {
 				// Malformed calculation, totals cannot exceed 100%
 				avg[stat] = 100.0
